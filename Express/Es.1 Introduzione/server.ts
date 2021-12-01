ba0edaaff6dd4ab43c7065ed84afa4f2ab1c1249
FabioCavallero <f.cavallero.1286@vallauri.edu>
2021-12-01 10:00:09 +0100

    Utilizzo delle prime route

--- Express/Es.1 Introduzione/server.ts
+++ Express/Es.1 Introduzione/server.ts
@@ -1,15 +1,59 @@
-import _express from "express";
-import * as _http from "http";
-import HEADERS from "./headers.json"; //Esportazione del file .json
+import express from "express";
+import * as fs from "fs";
+import * as http from "http";
+import * as body_parser from "body-parser";
+import { json } from "body-parser";
 let port: number=1337;
-let app=_express(); //Richiamo il costruttore
-let server=_http.createServer(app); //Routine che risponde alle richieste
-server.listen(port,function(){console.log("Server in ascolto sulla porta "+port)});
-//Elenco delle routes(listeners)
-app.use("*",function(req,res,next){
+let app=express(); //Richiamo il costruttore
+let server=http.createServer(app); //Routine che risponde alle richieste
+server.listen(port,function(){
+    console.log("Server in ascolto sulla porta "+port);
+    init();
+});
+let paginaErrore="";
+function init()
+{
+    fs.readFile("./static/error.html",function(err,data){
+        if(!err)
+            paginaErrore=data.toString();
+        else
+            paginaErrore="<h2>Risorsa non trovata</h2>";    
+    });
+}
+//Elenco delle route di tipo middleware
+//1.Log
+app.use("/",function(req,res,next){
     console.log("----->"+req.method+":"+req.originalUrl);
     next();
 });
-app.get("*",function(req,res,next){
-    res.send("This is the response");
+//2.Static route
+app.use("/",express.static("./static")); //se non trova la risorsa, il next() avviene in automatico
+//3.Route di lettura dei parametri post
+app.use("/",body_parser.json());
+app.use("/",body_parser.urlencoded({"extended":true}));
+//4.Log parametri
+app.use("/",function(req,res,next){
+    //Controllo se ci sono delle chiavi, se sì faccio il console.log
+    if(Object.keys(req.query).length>0) 
+        console.log("Parametri GET:",req.query);
+    if(Object.keys(req.body).length>0)
+        console.log("Parametri BODY: ",req.body);
+    next();
+})
+//Elenco delle route di risposta al client
+app.get("/api/risorsa1",function(req,res){
+    let nome=req.query.nome;
+    res.send({"nome":nome});
 });
+app.post("/api/risorsa1",function(req,res){
+    let nome=req.body.nome;
+    res.send({"nome":nome});
+});
+//Default route(risponde in caso di risorsa non trovata) e route di gestione degli errori
+app.use("/",function(req,res,next){
+    res.status(404);
+    if(req.originalUrl.startsWith("/api/"))
+        res.send("Risorsa non trovata");
+    else
+        res.send(paginaErrore);
+}) 
--- Express/Es.1 Introduzione/static/index.js
+++ Express/Es.1 Introduzione/static/index.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
     $("#btnInvia").on("click", function() {
-        let request = inviaRichiesta("POST", "/api/servizio1?id=5", {"nome":"pippo"});
+        let request = inviaRichiesta("GET", "/api/risorsa1", {"nome":"pippo"});
         request.fail(errore);
         request.done(function(data) {
             alert(JSON.stringify(data));
         });
     });
     $("#btnInvia2").on("click", function() {
-        let request = inviaRichiesta("GET", "/api/servizio2", {"nome":"pluto"});
+        let request = inviaRichiesta("POST", "/api/risorsa1", {"nome":"pluto"});
         request.fail(errore);
         request.done(function(data) {
             alert(JSON.stringify(data));
