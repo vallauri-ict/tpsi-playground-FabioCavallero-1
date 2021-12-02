48cf0d72de244823099bda34dd076904439c50db
FabioCavallero <f.cavallero.1286@vallauri.edu>
2021-12-02 11:41:24 +0100

    Concluso esercizio iniziale express

--- Express/Es.1 Introduzione/static/index.js
+++ Express/Es.1 Introduzione/static/index.js
@@ -1,16 +1,27 @@
 $(document).ready(function() {
+    //2 servizi possono avere lo stesso nome solo se hanno 2 chiamate differenti (GET e POST)
     $("#btnInvia").on("click", function() {
-        let request = inviaRichiesta("GET", "/api/risorsa1", {"nome":"pippo"});
+        let request = inviaRichiesta("GET", "/api/risorsa1", {"nome":"Kenny"});
         request.fail(errore);
         request.done(function(data) {
             alert(JSON.stringify(data));
         });
     });
     $("#btnInvia2").on("click", function() {
-        let request = inviaRichiesta("POST", "/api/risorsa1", {"nome":"pluto"});
+        let request = inviaRichiesta("PATCH", "/api/risorsa2", {"nome":"Unico","vampires":3});
         request.fail(errore);
         request.done(function(data) {
-            alert(JSON.stringify(data));
+            if(data.modifiedCount>0)
+                alert("Aggiornamento eseguito correttamente");
+            else
+                alert("Nessuna corrispondenza trovata");
+        });
+    });
+    $("#btnInvia3").on("click", function() {
+        let request = inviaRichiesta("GET", "/api/risorsa3/m/brown");
+        request.fail(errore);
+        request.done(function(data) {
+            console.log(data);
         });
     });
 });
