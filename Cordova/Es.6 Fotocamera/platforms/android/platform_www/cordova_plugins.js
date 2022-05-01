6c0082d1fcb3d05f5e84b245f4c3f77a6ef5de04
FabioCavallero <f.cavallero.1286@vallauri.edu>
2022-05-01 19:27:51 +0200

    Apportate alcune modifiche

--- Cordova/Es.6 Fotocamera/platforms/android/platform_www/cordova_plugins.js
+++ Cordova/Es.6 Fotocamera/platforms/android/platform_www/cordova_plugins.js
@@ -25,24 +25,41 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       ]
     },
     {
-      "id": "cordova-plugin-geolocation.geolocation",
-      "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
-      "pluginId": "cordova-plugin-geolocation",
+      "id": "cordova-plugin-camera.Camera",
+      "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+      "pluginId": "cordova-plugin-camera",
       "clobbers": [
-        "navigator.geolocation"
+        "Camera"
       ]
     },
     {
-      "id": "cordova-plugin-geolocation.PositionError",
-      "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
-      "pluginId": "cordova-plugin-geolocation",
-      "runs": true
+      "id": "cordova-plugin-camera.CameraPopoverOptions",
+      "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "CameraPopoverOptions"
+      ]
+    },
+    {
+      "id": "cordova-plugin-camera.camera",
+      "file": "plugins/cordova-plugin-camera/www/Camera.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "navigator.camera"
+      ]
+    },
+    {
+      "id": "cordova-plugin-camera.CameraPopoverHandle",
+      "file": "plugins/cordova-plugin-camera/www/CameraPopoverHandle.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "CameraPopoverHandle"
+      ]
     }
   ];
   module.exports.metadata = {
     "cordova-plugin-dialogs": "2.0.2",
-    "cordova-plugin-vibration": "3.1.1",
-    "cordova-plugin-splashscreen": "6.0.0",
-    "cordova-plugin-geolocation": "4.1.0"
+    "cordova-plugin-splashscreen": "6.0.1",
+    "cordova-plugin-camera": "6.0.0"
   };
 });
